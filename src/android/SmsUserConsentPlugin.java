eb9322a7c83a0dbc32f30a6c946b97e6d6237344
Vivek Patel <vivek.patel371@gmail.com>
2022-03-08 22:36:06 +0530

    Added method for  register and unregister device

--- src/android/SmsUserConsentPlugin.java
+++ src/android/SmsUserConsentPlugin.java
@@ -1,6 +1,19 @@
 package com.cordova.plugin;
 
-import android.util.Log;
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -8,23 +21,18 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.apache.cordova.*;
 
-import de.idnow.core.IDnowConfig;
-import de.idnow.core.IDnowResult;
-import de.idnow.core.IDnowSDK;
 
-public class SmsUserConsentPlugin extends CordovaPlugin implements IDnowSDK.IDnowResultListener {
-    private static String TAG = IdNowPlugin.class.getSimpleName();
-    private IDnowSDK idnowSdk;
-    private static final String  START_IDENT="START_IDENT";
+public class SmsUserConsentPlugin extends CordovaPlugin {
+    private static final int REQ_USER_CONSENT = 200;
+    private static final String START_LISTNING = "START_LISNING";
+    private static final String STOP_LISTNING = "STOP_LISTNING";
+    Activity mActivity;
+    SmsBroadcastReceiver smsBroadcastReceiver;
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        IDnowConfig idnowConfig = IDnowConfig.Builder.getInstance()
-                .withLanguage("en") // this line is not necessary, please see below
-                .build();
-
-        idnowSdk = IDnowSDK.getInstance();
-        idnowSdk.initialize(cordova.getActivity(), idnowConfig);
+        this.mActivity = cordova.getActivity();
 
     }
 
@@ -32,43 +40,65 @@ public class SmsUserConsentPlugin extends CordovaPlugin implements IDnowSDK.IDno
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 
-        if (START_IDENT.equals(action)) {
-            this.cordova.getActivity().runOnUiThread(new Runnable() {
-
-
-                                                         @Override
-                                                         public void run() {
-                                                             try {
-                                                                 startIdent(callbackContext,args.getString(0));
-                                                             } catch (JSONException e) {
-                                                                 e.printStackTrace();
-                                                             }
-                                                         }
-                                                     });
-
-
-
+        if (START_LISTNING.equals(action)) {
+            startSmsUserConsent();
             return true;
         }
-
-return false;
+        if (STOP_LISTNING.equals(action)) {
+            startSmsUserConsent();
+            return true;
+        }
+        return false;
     }
 
-    private void startIdent(CallbackContext callbackContext, String token) {
-        idnowSdk.startIdent(token, this);
+    private void startSmsUserConsent() {
+        registerBroadcastReceiver();
 
-        //idnowSdk.startIdent("PNW-JNTUR", this);
-        callbackContext.success();
+        SmsRetrieverClient client = SmsRetriever.getClient(mActivity);
+        //We can add sender phone number or leave it blank
+        // I'm adding null here
+        client.startSmsUserConsent(null).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(mActivity, "On Success", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(mActivity, "On OnFailure", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void registerBroadcastReceiver() {
+        smsBroadcastReceiver = new SmsBroadcastReceiver();
+        smsBroadcastReceiver.smsBroadcastReceiverListener =
+                new SmsBroadcastReceiver.SmsBroadcastReceiverListener() {
+                    @Override
+                    public void onSuccess(Intent intent) {
+                        mActivity.startActivityForResult(intent, REQ_USER_CONSENT);
+                    }
+
+                    @Override
+                    public void onFailure() {
+
+                    }
+                };
+        IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+        mActivity.registerReceiver(smsBroadcastReceiver, intentFilter);
     }
 
     @Override
-    public void onIdentResult(IDnowResult iDnowResult) {
-        if (iDnowResult.getIDnowStatusCode() == IDnowResult.IDnowStatusCode.FINISHED) {
-            Log.d(TAG, "Finished");
-        } else if (iDnowResult.getIDnowStatusCode() == IDnowResult.IDnowStatusCode.CANCELLED) {
-            Log.d(TAG, "Cancelled");
-        } else if (iDnowResult.getIDnowStatusCode() == IDnowResult.IDnowStatusCode.ERROR) {
-            Log.d(TAG, "Error: " + iDnowResult.getMessage());
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_USER_CONSENT) {
+            if ((resultCode == RESULT_OK) && (data != null)) {
+                //That gives all message to us.
+                // We need to get the code from inside with regex
+                String message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE);
+                Toast.makeText(mActivity, message, Toast.LENGTH_LONG).show();
+
+            }
         }
     }
 }
--- www/sms_user_consent.js
+++ www/sms_user_consent.js
@@ -1,19 +1,26 @@
+cordova.define("cordova-plugin-sms.sms_user_consent", function(require, exports, module) {
 
-function SmsUserConsentPlugin() {
-
-}
-//Send Data Plugin
-SmsUserConsentPlugin.prototype.sendData = function (dataToSend, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, 'IdNowPlugin', 'START_IDENT',[dataToSend]);
-};
-
-SmsUserConsentPlugin.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
+  function SmsUserConsentPlugin() {
+  
   }
-
-  window.plugins.smsUserConsentPlugin = new SmsUserConsentPlugin();
-  return window.plugins.smsUserConsentPlugin;
-};
-
-cordova.addConstructor(SmsUserConsentPlugin.install);
+  //Send Data Plugin
+  SmsUserConsentPlugin.prototype.registerDevice = function (dataToSend, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'SmsUserConsentPlugin', 'START_LISNING',[]);
+  };
+  SmsUserConsentPlugin.prototype.unregisterDevice = function (dataToSend, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'SmsUserConsentPlugin', 'STOP_LISTNING',[]);
+  };
+  
+  SmsUserConsentPlugin.install = function () {
+    if (!window.plugins) {
+      window.plugins = {};
+    }
+  
+    window.plugins.smsUserConsentPlugin = new SmsUserConsentPlugin();
+    return window.plugins.smsUserConsentPlugin;
+  };
+  
+  cordova.addConstructor(SmsUserConsentPlugin.install);
+  
+  });
+  
