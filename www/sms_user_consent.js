f2b67d9fc174d1f752e066b5bbbfa377c366bf9a
Vivek Patel <vivek.patel371@gmail.com>
2022-03-10 15:59:32 +0530

    minor change

--- www/sms_user_consent.js
+++ www/sms_user_consent.js
@@ -1,25 +1,6 @@
+var exec = require('cordova/exec');
 
-  function SmsUserConsentPlugin() {
-  
-  }
-  //Send Data Plugin
-  SmsUserConsentPlugin.prototype.registerDevice = function ( successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SmsUserConsentPlugin', 'START_LISNING',[]);
-  };
-  SmsUserConsentPlugin.prototype.unregisterDevice = function ( successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SmsUserConsentPlugin', 'STOP_LISTNING',[]);
-  };
-  
-  SmsUserConsentPlugin.install = function () {
-    if (!window.plugins) {
-      window.plugins = {};
-    }
-  
-    window.plugins.smsUserConsentPlugin = new SmsUserConsentPlugin();
-    return window.plugins.smsUserConsentPlugin;
-  };
-  
-  cordova.addConstructor(SmsUserConsentPlugin.install);
-  
-  
-  
+exports.registerDevice = function (success, error) {
+   exec(success, error, 'SmsUserConsentPlugin', 'START_LISNING');
+};
+
